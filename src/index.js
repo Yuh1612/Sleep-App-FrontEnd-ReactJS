68d6b82187b0b9fcb8d380466fd2231a9d312aad
Tran Huy <huytran161201@gmail.com>
2022-06-13 22:07:25 +0700

    react

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Login from "./Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Login />} />
+          <Route path="admin" element={<App />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
